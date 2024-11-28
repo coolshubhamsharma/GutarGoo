16b38e868637687dbc069d1099bd3b926cdc20cf
Shubham Sharma <coolshubhamsharma26@gmail.com>
2024-11-28 14:14:25 +0530

    configured for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,33 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+
 import authRoutes from "./routes/auth.js"
 import messageRoutes from "./routes/message.js"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+
 import { app, server } from "./socket/socket.js";
-
-
+import dotenv from "dotenv"
 dotenv.config();
 
+
 const port = process.env.PORT || 8080; 
+const __dirname = path.resolve();
 
 app.use(cookieParser()); //for parsing cookies
 app.use(express.json()); //used for parsing the application/json data
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+
+//using only one server for both backend and frontend in production environment
+if(process.env.NODE_ENV !== "development"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*" , (req, res)=>{
+        res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
+    })
+}
 
 
 server.listen(port , ()=>{ //using the http server
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Sidebar from '../../components/Sidebar/Sidebar'
 import MessageContainer from '../../components/Messages/MessageContainer'
 
